b4c457adcdc49dffbcdc854f9b9f348372084b57
Edward Clark <eeclark@gmail.com>
2015-01-20 16:40:03 -0600

    added calc

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var dogYearsLabel: UILabel!
+    @IBOutlet weak var humanYearsTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
+        let humanYearsFromTextField = humanYearsTextField.text.toInt()!
+        dogYearsLabel.hidden = false
+        dogYearsLabel.text = "\(humanYearsFromTextField * 7)"
+    
+    }
 
+    
 }
 
