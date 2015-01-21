29a6b55e671550ae1f7ab342b5cfe959ef6ca6ad
Edward Clark <eeclark@gmail.com>
2015-01-20 19:03:04 -0600

    added real age calc option

--- AgeOfLaika/ViewController.swift
+++ AgeOfLaika/ViewController.swift
@@ -24,6 +24,28 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
+    @IBAction func convertToRealDogYearsButtonPressed(sender: UIButton) {
+        if humanYearsTextField.text == "" {
+            pleaseEnterAValue.hidden = false
+            dogYearsLabel.hidden = true
+        }
+        else
+        {
+        var realDogYears = Double((humanYearsTextField.text as NSString).doubleValue)
+        if realDogYears <= 2 {
+            realDogYears = realDogYears * 10.5
+        } else {
+            realDogYears = (10.5 * 2) + (realDogYears - 2) * 4
+        }
+        dogYearsLabel.text = "\(realDogYears)" + " real dog years"
+        dogYearsLabel.hidden = false
+        pleaseEnterAValue.hidden = true
+        humanYearsTextField.resignFirstResponder()
+        }
+    
+    }
+
     @IBAction func convertToDogYearsButtonPressed(sender: UIButton) {
         if humanYearsTextField.text == "" {
         pleaseEnterAValue.hidden = false
@@ -43,7 +65,11 @@ class ViewController: UIViewController {
     @IBAction func clearButtonPressed(sender: UIButton) {
         dogYearsLabel.text = ""
         humanYearsTextField.text = ""
+        pleaseEnterAValue.hidden = true
     }
+    
+  
+
     
 }
 
